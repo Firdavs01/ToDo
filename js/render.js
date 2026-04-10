2134e38d2ca21a20cfb9cbe20b3d9ef18738fbc2
Firdavs <firdavsstandoff@gmail.com>
2026-04-10 19:15:31 +0500

    added delete option

--- js/render.js
+++ js/render.js
@@ -4,15 +4,32 @@ import { tasks } from "./structure.js";
 import { displayListElement } from "./elements.js";
 
 function render() {
-    displayListElement.innerHTML = ``
+  displayListElement.innerHTML = ``;
+
   tasks.forEach((task) => {
-    const li = document.createElement("li")
+    const li = document.createElement("li");
+    const delBtn = document.createElement('button');
+    delBtn.classList.add('delete__btn');
+    delBtn.textContent = "delete";
+
     li.textContent = task.value
 
     if (task.completed) {
-        li.classList.add('completed')
+      li.classList.add('completed')
     }
 
+    delBtn.addEventListener("click", () => {
+
+      const index = tasks.findIndex(t => t.id === task.id)
+
+      if (index !== -1) {
+        tasks.splice(index, 1)
+      }
+
+      render()
+    })
+    
+    li.appendChild(delBtn)
     displayListElement.appendChild(li)
   });
 }
