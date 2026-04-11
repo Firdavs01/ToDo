f3007799b47d75ca226ad16edfc41e0b0403d5c9
Firdavs <firdavsstandoff@gmail.com>
2026-04-11 18:37:09 +0500

    added enter click

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 "use strict";
 
 import { render } from "./render.js";
+import { inputElement, addBtnElement } from "./elements.js";
 
-render()
+
+console.log(inputElement, 'working')
+
+inputElement.addEventListener("keydown", (e) => {
+    console.log("Ola")
+    if (e.key === 'Enter') {
+        addBtnElement.click()
+        render()
+    }
+})
